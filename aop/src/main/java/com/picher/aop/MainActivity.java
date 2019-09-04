001e3d0966801b319cf77af2db91292b3d5b4d8d
picher <Xiuhao_Mo.sgmwh@saic-gm.com>
2019-09-04 12:46:18 +0800

    add premission

--- aop/src/main/java/com/picher/aop/MainActivity.java
+++ aop/src/main/java/com/picher/aop/MainActivity.java
@@ -1,12 +1,20 @@
 package com.picher.aop;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,8 +22,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+/**
+ * AOP Demo
+ * 以文件IO为例
+ */
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_ASK_STORAGE = 001;
     private TextView mContenttv;
     private Button mContentBt;
 
@@ -34,6 +47,55 @@ public class MainActivity extends AppCompatActivity {
                 writeFile();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int check = ContextCompat.checkSelfPermission(this, Manifest.permission_group.STORAGE);
+        if (check != PackageManager.PERMISSION_GRANTED) {
+            //requestPermission();
+        }
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission_group.STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("提示")
+                    .setMessage("需要开启存储的权限，是否继续？")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission_group.STORAGE}, REQUEST_CODE_ASK_STORAGE);
+                        }
+                    })
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+        } else {
+            //请求权限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE_ASK_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d("picher", "strings:" + permissions + "->>results:" + grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("picher", "授权同意!");
+                } else {
+                    Toast.makeText(MainActivity.this, "拒绝授权！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void writeFile() {
