7e7186a8f29043a862e9497c8eafb905e0734442
picher <Xiuhao_Mo.sgmwh@saic-gm.com>
2019-10-30 16:27:02 +0800

    delete key

--- iorecord/src/main/assets/change.sh
+++
@@ -1,2 +0,0 @@
-java -jar -Xmx512m -Djava.library.path="out/host/linux-x86/lib64" out/host/linux-x86/framework/signapk.jar build/target/product/security/platform.x509.pem build/target/product/security/platform.pk8 app-debug.apk ./LogCollectService/LogCollectService.apk
-
